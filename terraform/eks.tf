52310127aa29227d0974ef08ea16a392287d3cb2
sakthiPrabha58 <sakthiprabha58@gmail.com>
2026-09-17 12:14:40 +0530

    Remove SSM managed instance policy dependency

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -81,6 +81,7 @@ resource "aws_eks_node_group" "main" {
     aws_iam_role_policy_attachment.eks_worker_node_policy,
     aws_iam_role_policy_attachment.eks_cni_policy,
     aws_iam_role_policy_attachment.eks_ecr_read_only,
-    aws_iam_role_policy_attachment.ssm_managed_instance_core,
   ]
 }
+
+
